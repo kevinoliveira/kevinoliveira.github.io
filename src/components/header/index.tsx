4a3c6e5bfd9171e1243dbcf2b82f4c016e05f616
Kevin Oliveira <kevin.oliveira@live.com>
2018-09-16 16:51:01 -0300

    update header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,27 +2,19 @@ import * as React from 'react'
 import Link from 'gatsby-link'
 import "./index.scss"
 
-// const men
-
-
-// const Header = () => (
-
-
-//     // <div className="headerWrapper">
-//         <div className="header">
-//             <div className="titleWrapper">
-//                 <Link to="/"><mark>Ho</mark>me</Link>
-//             </div>
-//             <div className="NavWrapper">
-//                 <Link to="/blog/test">Menu</Link>
-//             </div>
-//       </div>
-//     // </div>
-// )
-
 interface State {
     menuOpen: boolean;
 }
+
+interface IMenuItem {
+    key: string;
+    to: string;
+    title: string | JSX.Element;
+    external?: boolean;
+    target?: string;
+    rel?: string;
+}
+
 class Header extends React.Component<{}, State> {
     state = {menuOpen: false};
 
@@ -33,10 +25,8 @@ class Header extends React.Component<{}, State> {
 
     render(){
 
-        const menuItens = [
+        const menuItens: IMenuItem[] = [
             {key:"blog" ,to: "/blog",title:"blog"},
-            // {key:"/blog/test" ,to: "/blog/test",title:"test"},
-            // {key:"/rss" ,to: "/rss",title:"rss"},
             {key:"contact" ,to: "/contact",title:"contact"},
             {external: true,key:"github" ,to: "https://www.github.com/kevinoliveira",target: "_blank",title:"Github"},
         ]
@@ -48,7 +38,6 @@ class Header extends React.Component<{}, State> {
         return (
             <div className="header">
                 <div className="title-wrapper">
-                    {/* <Link to="/"><mark>Ke</mark>vin Oliveira</Link> */}
                     <Link to="/">kevinoliveira.github.io</Link>
                 </div>
                 <div className="nav-wrapper-desktop">
