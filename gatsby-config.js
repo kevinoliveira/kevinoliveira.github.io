c4e7c8833e6f7641a3f2931e503d2fdcdefb17b9
Kevin Oliveira <kevin.oliveira@live.com>
2018-09-16 17:43:02 -0300

    favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,39 @@ module.exports = {
         name: "markdown-pages",
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+  
+        // WebApp Manifest Configuration
+        appName: null,
+        appDescription: null,
+        developerName: null,
+        developerURL: null,
+        dir: 'auto',
+        lang: 'en-150',
+        background: '#27ae60',
+        theme_color: '#27ae60',
+        display: 'standalone',
+        orientation: 'any',
+        start_url: '/',
+        version: '1.0',
+  
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          opengraph: false,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
     `gatsby-transformer-remark`,
     `gatsby-plugin-sass`,
   ],
