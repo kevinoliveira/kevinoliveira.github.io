41bb5fdcfbabe039358adb19dbe6b691040d71b9
Kevin Oliveira <kevin.oliveira@live.com>
2018-12-22 16:28:06 -0300

    responsive grid template

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -31,7 +31,7 @@ class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
           meta={[
             { name: "description", content: "Kevin Oliveira blog " },
             { name: "keywords", content: "sample, something" },
-            { name: "theme-color", content: "#2f4f4f" }
+            { name: "theme-color", content: "#f5f5f5" }
           ]}
         />
         {itens}
