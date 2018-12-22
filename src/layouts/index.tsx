313355bbd51c9b20ecd41da0d69ae66ec95977fd
Kevin Oliveira <kevin.oliveira@live.com>
2018-12-22 16:04:39 -0300

    new grid template

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
 import Helmet from "react-helmet";
 import "./index.scss";
-import Header from "../components/header";
-import Footer from "../components/footer";
-import Link from "gatsby-link";
-import InformationCards, { Item } from "../components/informationCards";
 
 interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
   data: {
@@ -19,15 +15,17 @@ interface DefaultLayoutProps extends React.HTMLProps<HTMLDivElement> {
 
 class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
   public render() {
-    const items: Item[] = [
-      // { legend: "BTC", value: "1DECAF2uSpFTP4L1fAHR8GCLrPqdwdLse9" },
-      // { legend: "ETC", value: "0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D" }
-      { legend: "POLITICAL", value: "anarcho-capitalist" },
-      { legend: "FREEDOM", value: "FIGHTER" }
-    ];
+    const n = 2000;
+    const arr = Array.apply(null, Array(n));
+
+    console.log(arr.length);
+    const itens = arr.map((_a, i) => {
+      console.log(i);
+      return <div className="box" key={i} >{i}</div>;
+    });
 
     return (
-      <div>
+      <div className="main-grid">
         <Helmet
           title="Kevin Oliveira"
           meta={[
@@ -36,33 +34,7 @@ class DefaultLayout extends React.PureComponent<DefaultLayoutProps, void> {
             { name: "theme-color", content: "#2f4f4f" }
           ]}
         />
-
-        <div className="background-top" />
-        <div className="children-wrapper">
-          {/* <Header /> */}
-          {/* <div className="children-card">{this.props.children()}</div> */}
-          {/* {this.props.children()} */}
-          {/* <Footer /> */}
-          <div className="page-title-grid">
-            <Link to="/">
-              <h1>{this.props.data.site.siteMetadata.title}</h1>
-            </Link>
-          </div>
-          <div className="links-grid">
-            <Header />
-          </div>
-          <div className="information-grid">
-            <InformationCards items={items} />
-          </div>
-          {/* <div className="example-3 title-grid">
-            <h1>{window.document.title}</h1>
-          </div> */}
-          <div className="example content-grid">
-            {/* content-grid */}
-            {this.props.children()}
-          </div>
-          {/* <div className="example six">Six</div> */}
-        </div>
+        {itens}
       </div>
     );
   }
