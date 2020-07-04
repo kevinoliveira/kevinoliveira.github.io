7d316d6d62d3935fd7bd7587c720f7a6e898bfc8
Kevin Olivera <kevin.oliviera@live.com>
2020-07-04 13:48:34 -0300

    vscode config and remove files

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ console.clear();
 console.log(new Date().toString())
 
 import {
-  IPage,
-  TemplatePost,
-  ParsedPost
+	IPage,
+	TemplatePost,
+	ParsedPost
 } from "./types";
 import fs from "fs";
 import path from "path";
@@ -18,84 +18,84 @@ import sass from "node-sass";
 
 // nunjunks setup
 const n = nunjucks.configure(["src"], {
-  autoescape: true,
-  noCache: true
+	autoescape: true,
+	noCache: true
 });
 
 // parsing posts
 const postsFilePaths = find("posts", { matching: "**/*.md" })
 const postsParsedData: ParsedPost[] = postsFilePaths.map(path => {
-  const fileContent = read(path, "utf8");
-  const FM = frontmatter(fileContent);
+	const fileContent = read(path, "utf8");
+	const FM = frontmatter(fileContent);
 
-  return {
-    headers: FM.data,
-    content: FM.content,
-    parsedHtml: marked(FM.content)
-  }
+	return {
+		headers: FM.data,
+		content: FM.content,
+		parsedHtml: marked(FM.content)
+	}
 });
 postsParsedData.sort((postA, postB) => {
-  const dateA = postA.headers.date;
-  const dateB = postB.headers.date;
-  return dateB.localeCompare(dateA, 'en', { sensitivity: 'base' });
+	const dateA = postA.headers.date;
+	const dateB = postB.headers.date;
+	return dateB.localeCompare(dateA, 'en', { sensitivity: 'base' });
 })
 
 // define files to compiple
 const pagesToCreate: IPage[] = [
-  {
-    htmlOutputName: "index.html",
-    cssOutputName: "index.css",
-    nunjunksTemplate: "pages/home/home.njk",
-    sassFile: "pages/home/main.scss",
-    context: {
-      hideHome: true,
-      posts: postsParsedToTemplate(postsParsedData)
-      // name: "Jake",
-      // age: 31,
-    }
-  },
-  {
-    cssOutputName: "404.css",
-    htmlOutputName: "404.html",
-    nunjunksTemplate: "pages/404/404.njk",
-    sassFile: "pages/404/404.scss",
-    context: { title: "404", message: "Sorry, can't find anything" }
-  },
-  {
-    cssOutputName: "artlets.css",
-    htmlOutputName: "artlets.html",
-    nunjunksTemplate: "pages/404/404.njk",
-    sassFile: "pages/404/404.scss",
-    context: { title: "Artlets", message: "Artlets" }
-  },
-  {
-    cssOutputName: "projects.css",
-    htmlOutputName: "projects.html",
-    nunjunksTemplate: "pages/404/404.njk",
-    sassFile: "pages/404/404.scss",
-    context: { title: "Projects", message: "Projects" }
-  },
-  {
-    cssOutputName: "blog.css",
-    htmlOutputName: "blog.html",
-    nunjunksTemplate: "pages/404/404.njk",
-    sassFile: "pages/404/404.scss",
-    context: { title: "Blog", message: "Blog" }
-  },
-  {
-    cssOutputName: "about.css",
-    htmlOutputName: "about.html",
-    nunjunksTemplate: "pages/404/404.njk",
-    sassFile: "pages/404/404.scss",
-    context: { title: "About Me", message: "About Me" }
-  },
-  {
-    cssOutputName: "secret.css",
-    htmlOutputName: "secret.html",
-    nunjunksTemplate: "pages/404/404.njk",
-    sassFile: "pages/404/404.scss",
-    context: { title: "Secret", message: "Secret page" }
-  }
+	{
+		htmlOutputName: "index.html",
+		cssOutputName: "index.css",
+		nunjunksTemplate: "pages/home/home.njk",
+		sassFile: "pages/home/main.scss",
+		context: {
+			hideHome: true,
+			posts: postsParsedToTemplate(postsParsedData)
+			// name: "Jake",
+			// age: 31,
+		}
+	},
+	{
+		cssOutputName: "404.css",
+		htmlOutputName: "404.html",
+		nunjunksTemplate: "pages/404/404.njk",
+		sassFile: "pages/404/404.scss",
+		context: { title: "404", message: "Sorry, can't find anything" }
+	},
+	{
+		cssOutputName: "artlets.css",
+		htmlOutputName: "artlets.html",
+		nunjunksTemplate: "pages/404/404.njk",
+		sassFile: "pages/404/404.scss",
+		context: { title: "Artlets", message: "Artlets" }
+	},
+	{
+		cssOutputName: "projects.css",
+		htmlOutputName: "projects.html",
+		nunjunksTemplate: "pages/404/404.njk",
+		sassFile: "pages/404/404.scss",
+		context: { title: "Projects", message: "Projects" }
+	},
+	{
+		cssOutputName: "blog.css",
+		htmlOutputName: "blog.html",
+		nunjunksTemplate: "pages/404/404.njk",
+		sassFile: "pages/404/404.scss",
+		context: { title: "Blog", message: "Blog" }
+	},
+	{
+		cssOutputName: "about.css",
+		htmlOutputName: "about.html",
+		nunjunksTemplate: "pages/404/404.njk",
+		sassFile: "pages/404/404.scss",
+		context: { title: "About Me", message: "About Me" }
+	},
+	{
+		cssOutputName: "secret.css",
+		htmlOutputName: "secret.html",
+		nunjunksTemplate: "pages/404/404.njk",
+		sassFile: "pages/404/404.scss",
+		context: { title: "Secret", message: "Secret page" }
+	}
 ]
 
 
@@ -106,12 +106,22 @@ dir("output/posts")
 
 // html
 postsParsedData.forEach(post => {
-  const html = n.render("pages/post/post.njk", { rawHtml: post.parsedHtml, title: post.headers.title });
-  fs.writeFileSync(path.join("output/posts/", post.headers.key + ".html",), html, "UTF-8");
+	const html = n.render(
+		"pages/post/post.njk",
+		{
+			rawHtml: post.parsedHtml,
+			title: post.headers.title
+		});
+	fs.writeFileSync(
+		path.join("output/posts/", post.headers.key + ".html",),
+		html,
+		"UTF-8");
 });
 
 // styles
-const css = sass.renderSync({ file: "src/pages/post/post.scss" }).css.toString();
+const css = sass.renderSync(
+	{ file: "src/pages/post/post.scss" }
+).css.toString();
 fs.writeFileSync(path.join("output/", "post.css"), css, "UTF-8");
 
 
@@ -119,13 +129,15 @@ fs.writeFileSync(path.join("output/", "post.css"), css, "UTF-8");
 
 // html
 pagesToCreate.forEach(p => {
-  const html = n.render(p.nunjunksTemplate, p.context);
-  fs.writeFileSync(path.join("output/", p.htmlOutputName), html, "UTF-8");
+	const html = n.render(p.nunjunksTemplate, p.context);
+	fs.writeFileSync(path.join("output/", p.htmlOutputName), html, "UTF-8");
 })
 // styles
 pagesToCreate.forEach(p => {
-  const css = sass.renderSync({ file: path.join("src/", p.sassFile) }).css.toString();
-  fs.writeFileSync(path.join("output/", p.cssOutputName), css, "UTF-8");
+	const css = sass.renderSync(
+		{ file: path.join("src/", p.sassFile) }
+	).css.toString();
+	fs.writeFileSync(path.join("output/", p.cssOutputName), css, "UTF-8");
 })
 
 
@@ -137,10 +149,10 @@ pagesToCreate.forEach(p => {
 
 
 function postsParsedToTemplate(parsed: ParsedPost[]): TemplatePost[] {
-  return parsed.map(p => ({
-    title: p.headers.title,
-    date: p.headers.date,
-    description: p.headers.description,
-    link: `/posts/${p.headers.key}`
-  }))
+	return parsed.map(p => ({
+		title: p.headers.title,
+		date: p.headers.date,
+		description: p.headers.description,
+		link: `/posts/${p.headers.key}`
+	}))
 }
